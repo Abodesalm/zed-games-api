89b6c147cbda7ee142f9cfff79f351e8d555dc4c
abod <abodesalm45@gmail.com>
2024-07-18 06:21:49 +0300

    page limit = 14

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
+//const DB = process.env.LOCAL_DATABASE;
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
--- utils/apifeatures.js
+++ utils/apifeatures.js
@@ -42,7 +42,7 @@ class APIFeatures {
   paginate() {
     // Pagination
     const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 20;
+    const limit = this.queryString.limit * 1 || 14;
     const skip = (page - 1) * limit;
     this.query = this.query.skip(skip).limit(limit);
     return this;
